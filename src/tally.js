3fc42144f94b381e148e735c9af56683bb64656d
Mandana <mandana.khademi@madetech.com>
2023-12-01 14:20:26 +0000

    Refactor tally function

--- src/tally.js
+++ src/tally.js
@@ -4,36 +4,14 @@ function tally(shiftsByUser) {
   Object.keys(shiftsByUser).forEach((user) => {
     const shifts = shiftsByUser[user];
 
-    const userTotals = {
-      totalShifts: 0,
-      weekendShifts: 0,
-      weekdayShifts: 0,
+    totals[user] = {
+      totalShifts: shifts.filter((shift) => !shift.isInHours).length,
+      weekendShifts: shifts.filter((shift) => shift.isWeekend || shift.isBankHoliday).length,
+      weekdayShifts: shifts.filter((shift) => !shift.isWeekend
+                                             && !shift.isBankHoliday
+                                             && !shift.isInHours).length,
+
     };
-
-    shifts.forEach((shift) => {
-      if (!shift.isInHours) {
-        userTotals.totalShifts += 1;
-      }
-      if (shift.isWeekend || shift.isBankHoliday) {
-        userTotals.weekendShifts += 1;
-      }
-      if (!shift.isWeekend && !shift.isBankHoliday && !shift.isInHours) {
-        userTotals.weekdayShifts += 1;
-      }
-    });
-
-    // totals[user] = {
-    // totalShifts: shifts.length,
-    // totalShifts: shifts.filter((shift) => !shift.isInHours).length
-    // totalShifts: shifts.length - shifts.filter((shift) => shift.isInHours).length,
-    // weekendShifts: shifts.filter((shift) => shift.isWeekend || shift.isBankHoliday).length,
-    // weekdayShifts: shifts.filter((shift) => !shift.isWeekend &&
-    //                                         !shift.isBankHoliday &&
-    //                                         !shift.isInHours).length,
-    //      weekdayShifts: shifts.filter((shift) => !shift.isWeekend &&
-    //                                              !shift.isBankHoliday).length,
-
-    totals[user] = userTotals;
   });
 
   return totals;
